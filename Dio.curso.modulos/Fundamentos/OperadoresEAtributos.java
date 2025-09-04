dac2b0d6769fdc7c4b468898bd8b2d6c2a701e94
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-04 15:33:56 -0300

    Commit implementação de metódos ou / e.

--- Dio.curso.modulos/Fundamentos/OperadoresEAtributos.java
+++ Dio.curso.modulos/Fundamentos/OperadoresEAtributos.java
@@ -3,10 +3,14 @@ import java.util.Scanner;
 public class OperadoresEAtributos{
     public static void main(String[] args){
         var scanner = new Scanner(System.in);
+
         System.out.println("Qual sua idade?");
-        var idade = scanner.nextInt();
-        System.out.println("");
-        var naoDirige = idade <= 17;
-        System.out.printf("Você pode dirigir? (%s)\n", !naoDirige); //caso eu queria mais de um argumento, uso "printf"
+        var age = scanner.nextInt();
+
+        System.out.println("Você é emancipado? (true/false)");
+        var isEmancipated = scanner.nextBoolean();
+        var canDrive = age >= 18 || isEmancipated && age >= 16;
+
+        System.out.printf("Você pode dirigir? (%s)\n", canDrive); //caso eu queria mais de um argumento, uso "printf"
     }
 }
