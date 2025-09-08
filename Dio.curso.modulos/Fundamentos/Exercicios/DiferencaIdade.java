56128040e6886ef4e70039cb2a0611538a9539d4
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-08 14:30:44 -0300

    commit questão para calcular a diferença de idade entre duas pessoas.

---
+++ Dio.curso.modulos/Fundamentos/Exercicios/DiferencaIdade.java
@@ -0,0 +1,33 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class DiferencaIdade {
+    Scanner scan = new Scanner(System.in);
+    String nome1, nome2;
+    int idade1, idade2, diferenca;
+
+    public void calcularDiferenca(){
+            System.out.print("Olá, informe seu nome: ");
+            nome1 = scan.nextLine();
+            System.out.print("Informe sua idade: ");
+            idade1 = scan.nextInt();
+
+            System.out.println("Agora informe o segundo nome: ");
+            nome2 = scan.nextLine();
+            System.out.print("Informe a segunda idade: ");
+            idade2 = scan.nextInt();
+
+            diferenca = idade1 - idade2;
+    }
+
+    public void imprimirDiferenca(){
+        System.out.println("A diferença de idade entre " + nome1 + " e " + nome2 + " ,é de " + diferenca + " anos.");
+    }
+
+    public static void main(String[] args){
+        DiferencaIdade diferencaIdade = new DiferencaIdade();
+            diferencaIdade.calcularDiferenca();
+            diferencaIdade.imprimirDiferenca();
+    }
+}
